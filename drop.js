34f63672d265116c4591719bd6946e92846df680
Namdak Tonpa <maxim@synrc.com>
2019-08-23 22:04:56 +0300

    drop.js

---
+++ drop.js
@@ -0,0 +1,11 @@
+
+function dropChap() {
+  document.getElementById("dropdownCHAPTER").classList.toggle("show");
+  document.getElementById("dropdownLANG").classList.remove("show");
+}
+
+function dropLang() {
+  document.getElementById("dropdownLANG").classList.toggle("show");
+  document.getElementById("dropdownCHAPTER").classList.remove("show");
+}
+
